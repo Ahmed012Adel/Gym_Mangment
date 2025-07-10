83a1b42ee3088a410405d77bd2005d70977c5033
Ahmed Adel <Ahmed_Adel_2415@fci.helwan.edu.eg>
2025-07-10 15:59:23 +0300

    Implement generic repository pattern

---
+++ GYMMAngment.Presistente/Reposteries/GenricRepositery.cs
@@ -0,0 +1,38 @@
+﻿using Gym.Mangment.Domain.Contract;
+using GYMMAngment.Presistente.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GYMMAngment.Presistente.Reposteries
+{
+    public class GenricRepositery<TEntity, TKey>(ApplicationContext context) : IGenericRepo<TEntity, TKey>
+        where TEntity : class
+        where TKey : IEquatable<TKey>
+    {
+        public async Task<IEnumerable<TEntity>> GetAllAsync(bool WithTracking = false)
+        {
+            return WithTracking ? await context.Set<TEntity>().ToListAsync() :
+                    await context.Set<TEntity>().AsNoTracking().ToListAsync();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(TKey id)
+        {
+            return await context.Set<TEntity>().FindAsync(id);
+        }
+                
+        public async Task AddAsync(TEntity entity)
+                =>await context.Set<TEntity>().AddAsync(entity);
+        
+        public void UpdateAsync(TEntity entity)
+        {
+            context.Set<TEntity>().Update(entity);
+            context.SaveChanges();
+        }
+        public void DeleteAsync(TEntity entity)
+                =>context.Set<TEntity>().Remove(entity);
+    }
+}
---
+++ Gym.Mangment.Domain/Contract/IGenericRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym.Mangment.Domain.Contract
+{
+    public interface IGenericRepo<TEntity,TKey>
+        where TEntity : class
+        where TKey : IEquatable<TKey>
+    {
+        public Task<TEntity?> GetByIdAsync(TKey id);
+        public Task<IEnumerable<TEntity>> GetAllAsync(bool WithTracking = false);
+        public Task AddAsync(TEntity entity);
+        public void UpdateAsync(TEntity entity);
+        public void DeleteAsync(TEntity entity);
+    }
+}
